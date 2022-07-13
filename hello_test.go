d363dbdd55b945340b3e41611f188039c8d2f192
Ryan Palumbo <rynpal@gmail.com>
2022-07-13 13:20:47 -0400

    add support for multiple languages

--- hello_test.go
+++ hello_test.go
@@ -12,13 +12,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World', when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Ryan", "French")
+		want := "Bonjour, Ryan"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Japanese", func(t *testing.T) {
+		got := Hello("Ryan", "Japanese")
+		want := "Konnichiwa, Ryan"
 		assertCorrectMessage(t, got, want)
 	})
 }
--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
+const japanese = "Japanese"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const japaneseHelloPrefix = "Konnichiwa, "
 
-func Hello(name string) string  {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
 
-func main()  {
-	fmt.Println(Hello("world"))
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case japanese:
+		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("", ""))
 }
