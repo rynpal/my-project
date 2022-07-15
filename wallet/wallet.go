1f76f066467f35121f5a72bae4386c9c79794359
Ryan Palumbo <rynpal@gmail.com>
2022-07-15 13:28:18 -0400

    add withdraw, nsf, assertbalance to testing create bitcoin stringer

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,18 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
-	w.balance += amount
+type Stringer interface {
+	String() string
 }
 
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+	w.balance -= amount
+	return nil
+}
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
