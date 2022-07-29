0b01f40389dec2188c68f8549c8abb47d49abbc9
Ryan Palumbo <rynpal@gmail.com>
2022-07-29 11:57:58 -0400

    pass in context to fetch method

---
+++ context/context.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+	// Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprint(w, data)
+	}
+}
