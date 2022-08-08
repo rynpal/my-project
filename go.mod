e905661c10e750dc6f39a46a2711e454b078f007
Ryan Palumbo <rynpal@gmail.com>
2022-08-08 13:26:59 -0400

    remove github package dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module hello
 
 go 1.18
 
-require github.com/quii/learn-go-with-tests v0.0.0-20220802064911-2f88956a0031
--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -4,30 +4,28 @@ import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/quii/learn-go-with-tests/math/v1/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
-
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.SecondHand(tm)
+// func TestSecondHandAtMidnight(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// 	want := Point{X: 150, Y: 150 - 90}
+// 	got := SecondHand(tm)
 
 // 	if got != want {
 // 		t.Errorf("Got %v, wanted %v", got, want)
 // 	}
 // }
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
 
 func TestSecondsInRadians(t *testing.T) {
 	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
