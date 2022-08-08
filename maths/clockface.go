3030245d3fd938896bb7da1ce1fa503c51863d54
Ryan Palumbo <rynpal@gmail.com>
2022-08-08 11:48:26 -0400

    add type and secondHand fn

--- maths/clockface.go
+++ maths/clockface.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-// type Point struct {
-// 	X float64
-// 	Y float64
-// }
+type Point struct {
+	X float64
+	Y float64
+}
 
-// func SecondHand(t time.Time) Point {
-// 	return Point{150,60}
-// }
+func SecondHand(t time.Time) Point {
+	return Point{150,60}
+}
 
 func secondsInRadians(t time.Time) float64 {
 	return math.Pi
